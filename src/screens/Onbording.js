a5f0d034689e5470b78ed05b4a201feb93b63aac
shivam2003sy <shivam2003sy@gmail.com>
2022-08-17 11:29:50 +0530

    end point

--- src/screens/Onbording.js
+++ src/screens/Onbording.js
@@ -4,15 +4,12 @@ import Box from "@mui/material/Box"
 import Projects from '../components/Projects'
 export const Onbording = () => {
     useEffect(() => {
-        axios.get("http://sih-hydrateq.herokuapp.com/")
+        axios.get(`http://127.0.0.1:5000`)
           .then(response =>{ setPro(response.data.projects)
         //   console.log(response.data.projects)
         });
       }, []);
     const [pro,setPro] = React.useState([]);
-    
-   
-    
     return (
         <Box
             display="flex"
